8b14835e418ac0e1bcd433c4ceb28c8feeb5e624
riyasharma2020 <98735990+riyasharma2020@users.noreply.github.com>
2022-02-20 22:32:20 +0530

    Add files via upload

---
+++ USE_CRUD_OPERATION.java
@@ -0,0 +1,24 @@
+package JDBC_codes;
+
+
+public class USE_CRUD_OPERATION {
+    public static void main(String args[]){
+        CRUD_OPERATION call=new CRUD_OPERATION();
+//        call.read();
+//        call.insert(2, "Jatin", "Kesarwani", "Chakghat", 2);
+//        call.read();
+//        call.updateCity(1, "Prayagraj");
+//        call.read();
+//        call.updateProjectID(2, 8);
+//        call.read();
+//        call.delete(2);
+//        System.out.println("After Deletion");
+//        call.read();
+//        call.getEmpProjID(1);
+//        call.getEmpCity(1);
+        call.getEmpName(4);
+            
+    }
+}
+
+
